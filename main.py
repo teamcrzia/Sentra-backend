9e63129e2ac8c5a75f444a28b85c0099faac528a
teamcrzia <team.crzia@gmail.com>
2026-04-14 18:37:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,7 @@ app = FastAPI()
 # ✅ CORS MUST BE HERE (RIGHT AFTER app)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
